b722b93ce9cd8611c6b50ddfb81b47724a1c7ffe
Dawit-y <dawityimer52@gmail.com>
2024-03-30 10:50:23 +0300

    Fix issue with user registering through admin panel.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,58 +1,53 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
 from .models import *
 
-# from django.contrib.auth.admin import UserAdmin
-# from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+User = get_user_model()
 
+class CustomUserCreationForm(UserCreationForm):
+    
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('email',)
 
-# class CustomUserCreationForm(UserCreationForm):
-#     """
-#     A form that creates a custom user with no privileges
-#     from a provided email and password.
-#     """
+class CustomUserChangeForm(UserChangeForm):
 
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
+    class Meta(UserChangeForm.Meta):
+        model = User
+        fields = '__all__'
 
-#     class Meta(UserCreationForm.Meta):
-#         model = User
-#         fields = ('email',)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+            (
+                None,
+                {
+                    'classes': ('wide',),
+                    'fields': ('email', 'first_name', 'last_name', 'password1', 'password2'),
+                },
+            ),
+        )
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+    list_display = ['email', 'first_name', 'last_name', 'is_staff']
+    ordering = ['email']
+    exclude = ['username']
 
-# class CustomUserChangeForm(UserChangeForm):
-#     """
-#     A form for updating users. Includes all the fields on
-#     the user, but replaces the password field with admin's
-#     password hash display field.
-#     """
+    def save_model(self, request, obj, form, change):
 
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         self.fields.pop('username')
+        obj.set_password(form.cleaned_data["password1"])
+        super().save_model(request, obj, form, change)
 
-#     class Meta(UserChangeForm.Meta):
-#         model = User
-#         fields = '__all__'
-
-# class CustomUserAdmin(UserAdmin):
-#     model = User
-#     fieldsets = (
-#         (None, {'fields': ('email', 'password')}),
-#         ('Personal Info', {'fields': ('first_name', 'last_name')}),
-#         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-#         ('Important dates', {'fields': ('last_login', 'date_joined')}),
-#     )
-#     form = CustomUserChangeForm
-#     add_form = CustomUserCreationForm
-#     list_display = ['email', 'first_name', 'last_name', 'is_staff']
-#     ordering = ['email']
-#     exclude = ['username']
-
-#     def save_model(self, request, obj, form, change):
-
-#         obj.set_password(form.cleaned_data["password"])
-#         super().save_model(request, obj, form, change)
-
-admin.site.register(User)
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Applicant)
 admin.site.register(Student)
 admin.site.register(Organization)
